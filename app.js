4537460810a7e3d4aeeffe525250e57202961646
Faiza Saleem <faizasaleem288@gmail.com>
2026-09-10 18:06:12 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,60 +1,17 @@
-// 
-var urdu = prompt("Your marks in urdu?")
-var eng = prompt("Your marks in eng?")
-var math = prompt("Your marks in math?")
-var totalMarks = 300
-var obtainedMarks = urdu + eng + math
-var percentage =( obtainedMarks * 100) / totalMarks
-if (urdu >100 && eng >100 && math >100){
-    console.log ("invaled numbers")
-}else if (urdu <0 && eng <0 && math <0){
-    console.log ("invaled numbers")
-}
-else {
-    if (percentage >=80 && percentage <=100){
-        grade = "A+"
-    }
-    else if (percentage >=70 && percentage <80){
-        grade = "A"
-    }
-    else if (percentage >=60 && percentage <70){
-        grade = "B"
-    }
-    else if (percentage >=50 && percentage <60){
-        grade = "C"
-    }
-    else if (percentage <50 && percentage >=0){
-        grade = "Fail"
-    }
-}
-document.write(`
-<table>
-<tr>
-    <th>Subject</th>
-    <th>total marks</th>
-    <th>Obtained Marks</th>
-</tr>
-<tr>
-    <td>eng</td>
-    <td>100</td>
-    <td>${eng}</td>
-</tr>
-<tr>
-    <td>urdu</td>
-    <td>100</td>
-    <td>${urdu}</td>
-</tr>
-<tr>
-    <td>math</td>
-    <td>100</td>
-    <td>${math}</td>
-</tr>
-<tr>
-    <td>total</td>
-    <td>300</td>
-    <td>${obtainedMarks}</td>
-</tr>
-</table>
-
-`)
-
+var students = ["micheal", "john", "tony"]
+var score = [320, 230, 480]
+var totalMarks = 500
+var percentage0 = (score[0]*100)/totalMarks
+var percentage1 = (score[1]*100)/totalMarks
+var percentage2 = (score[2]*100)/totalMarks
+document.write (` Score of  ${students[0]} is ${ score[0]}. Percentage: ${percentage0} % <br> Score of  ${students[1]} is ${ score[1]}. Percentage: ${percentage1} % <br> Score of  ${students[2]} is ${ score[2]}. Percentage: ${percentage2} % <br>`)
+var color = ["red", "yellow", "blue", "pink"]
+document.write (color + "<br>")
+var begin = prompt("Which color you like to put at the beginning?")
+color.unshift (begin)
+document.write (color+ "<br>")
+var end = prompt("Which color you like to put at the end?")
+color.push (end)
+document.write (color+ "<br>")
+color.unshift ("purple", "green")
+document.write (color+ "<br>")
